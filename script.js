fb402753de6f684598c019e8d2a6f0edb0b2f89b
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-26 18:53:25 +0100

    add card without img

--- script.js
+++ script.js
@@ -39,9 +39,32 @@ const teamMembers = [
 
 // get html element where add cards
 const cards_container_element = document.querySelector(".cards_container")
+// get from elements
+const name_surname_element = document.querySelector(".name-surname") // Name & surname field
+const role_element = document.querySelector(".role-value") // Role field
+const email_element = document.querySelector(".email-value") // Email field
+const img_element = document.querySelector(".img-value") // Image field
+const submit_btn_element = document.querySelector(".btn-submit") // submit button
+const reset_btn_element = document.querySelector(".btn-reset") // reset button
+const form_element = document.querySelector("form") // form
 
 // page initialization with elements in list
 add_elements_in_list(teamMembers)
+form_element.addEventListener("submit", TakeFields)
+//form_element.addEventListener("reset", ClearFields)
+// connect function with form
+function TakeFields(event){
+  event.preventDefault() //not provvide submit
+  let obj={
+    name: name_surname_element.value,
+    role: role_element.value,
+    email: email_element.value,
+    img: img_element
+  }
+  console.log(obj)
+  add_card(obj)
+  
+  }
 
 //functions to add cards into container
 
